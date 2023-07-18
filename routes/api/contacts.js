7f9f9c49ef27cf7bc016973f4d4ee9b4ce8b927c
Yuliya1913 <Lera-855@email.ua>
2023-07-18 16:34:34 +0300

    add data

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,33 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+// Создаем часть проекта(страницу), отвечающую за контакты
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// импортируем методы для работы с контактами
+const contacts = require("../../models/contacts/contacts");
+console.log(contacts);
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// Запрос на все книги,нужно получить все книги и их отправить
+router.get("/", async (req, res, next) => {
+  const result = await contacts.listContacts();
+  res.json(result);
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-module.exports = router
+router.put("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+// экспортируем часть проекта отвечающего за контакты
+module.exports = router;
